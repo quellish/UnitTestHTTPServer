9e0315788a150ca340cb08ad0005cf5c7cb18435
quellish <github@quellish.neomailbox.com>
2012-06-07 22:30:31 -0700

    Initial Commit

---
+++ UnitTestHTTPServer/UnitTestHTTPServer.h
@@ -0,0 +1,13 @@
+//
+//  UnitTestHTTPServer.h
+//  UnitTestHTTPServer
+//
+//  Created by Dan Zinngrabe on 6/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UnitTestHTTPServer : NSObject
+
+@end
---
+++ UnitTestHTTPServer/UnitTestHTTPServer.m
@@ -0,0 +1,13 @@
+//
+//  UnitTestHTTPServer.m
+//  UnitTestHTTPServer
+//
+//  Created by Dan Zinngrabe on 6/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UnitTestHTTPServer.h"
+
+@implementation UnitTestHTTPServer
+
+@end
